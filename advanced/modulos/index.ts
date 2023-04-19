2fe594c18d473ced88942504e15a8a85534d2ee2
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-04-19 14:31:09 -0500

    Manejo de modulos

---
+++ advanced/modulos/index.ts
@@ -0,0 +1,4 @@
+import { generateId, PI, User } from "./utils.module";
+const myNumber = 10 * PI;
+
+const myUser: User= { id: generateId().toString(), name: "Jorge"}
---
+++ advanced/modulos/utils.module.ts
@@ -0,0 +1,11 @@
+export const PI = 3.14;
+
+export interface User{
+    id:string;
+    name:string;
+}
+
+export const generateId = () => {
+    return Math.floor(Math.random() * 100);
+}
+
